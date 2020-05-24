697ea06ab1b8df16feb91b825d858da0e589102e
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2020-05-24 20:07:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+## RayCarrot.IO
+This library includes the `FileSystemPath` value type to easier manage file paths, along with IO related helpers.
+
+## Features
+- FileSystemPath value type
+- Temporary file/directory helpers
+- IO helper methods
+
+## Dependencies
+RayCarrot.IO uses these main dependencies:
+
+- [RayCarrot.Logging](https://github.com/RayCarrot/RayCarrot.Logging)
+- [RayCarrot.Common](https://github.com/RayCarrot/RayCarrot.Common)
+- [ByteSize](https://github.com/omar/ByteSize)
+
+## Contact
+You can contact me on the following places:
+
+- [Twitter](https://twitter.com/RayCarrot)
+- [Email](mailto:RayCarrotMaster@gmail.com)
+
+## Licence
+
+[MIT License (MIT)](./LICENSE)
---
+++ RayCarrot.IO/Temp/TempFile.cs
@@ -0,0 +1,68 @@
+﻿using RayCarrot.Logging;
+using System;
+using System.IO;
+
+namespace RayCarrot.IO
+{
+    /// <summary>
+    /// A temporary file
+    /// </summary>
+    public sealed class TempFile : TempFileSystemEntry
+    {
+        /// <summary>
+        /// Creates a new temporary file
+        /// </summary>
+        /// <param name="createFile">Indicates if the temporary file should be created</param>
+        public TempFile(bool createFile)
+        {
+            if (createFile)
+            {
+                // Get the temp path and create the file
+                TempPath = Path.GetTempFileName();
+
+                // Get the file info
+                var info = TempPath.GetFileInfo();
+
+                // Set the attribute to temporary
+                info.Attributes |= FileAttributes.Temporary;
+            }
+            else
+            {
+                // Get the temp path
+                FileSystemPath tempFile;
+
+                // Get a random temp path until one does not exist
+                do
+                {
+                    tempFile = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName() + ".tmp");
+                } while (tempFile.DirectoryExists);
+
+                // Set the temp path
+                TempPath = tempFile;
+            }
+
+            RL.Logger?.LogDebugSource($"A new temp file has been created under {TempPath}");
+        }
+
+        /// <summary>
+        /// The path of the temporary file
+        /// </summary>
+        public override FileSystemPath TempPath { get; }
+
+        /// <summary>
+        /// Removes the temporary file
+        /// </summary>
+        public override void Dispose()
+        {
+            try
+            {
+                // Delete the temp file
+                TempPath.DeleteFile();
+            }
+            catch (Exception ex)
+            {
+                ex.HandleError("Deleting temp file");
+            }
+        }
+    }
+}
